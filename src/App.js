0e60ace989e74f347c0a848685ed41055a256cda
Lee Ji Yoon <leejiyooniiiii@gmail.com>
2025-04-16 17:21:30 +0900

    tictacto finished

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({ value, onSquareClick }) {
     );
 }
 
-/*export default*/ function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay }) {
     // const [xIsNext, setXIsNext] = useState(true);
     // const [squares, setSquares] = useState(Array(9).fill(null));
     function handleClick(i) {
@@ -35,7 +35,8 @@ function Square({ value, onSquareClick }) {
     }
 
     return (
-        <><div className="status">{status}</div>
+        <>
+          <div className="status">{status}</div>
           <div className="board-row">
               <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
               <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -76,17 +77,19 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2  === 0;
+    const currentSquares = history[currentMove];
 
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
     }
 
     function jumpTo(nextMove) {
-
+        setCurrentMove(nextMove);
     }
 
     const moves = history.map((squares, move) => {
@@ -97,7 +100,7 @@ export default function Game() {
             description = 'Go to game start';
         }
         return (
-            <li>
+            <li key={move}>
                 <button onClick={() => jumpTo(move)}>{description}</button>
             </li>
         );
@@ -114,3 +117,9 @@ export default function Game() {
         </div>
     );
 }
+
+// 1. 현재 이동에 대해서만 버튼 대신 “당신은 #번째 순서에 있습니다…”를 표시해 보세요.
+// 2. Board를 하드 코딩 하는 대신 두 개의 루프를 사용하여 사각형을 만들도록 다시 작성해 보세요.
+// 3. 동작을 오름차순 또는 내림차순으로 정렬할 수 있는 토글 버튼을 추가해 보세요.
+// 4. 누군가 승리하면 승리의 원인이 된 세 개의 사각형을 강조 표시해 보세요. (아무도 승리하지 않으면 무승부라는 메시지를 표시하세요. )
+// 5. 이동 히스토리 목록에서 각 이동의 위치를 형식(열, 행)으로 표시해 보세요.
